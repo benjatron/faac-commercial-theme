fa35027840b1f26c308313d0d595c4a6efe5d54b
Ben Davidson <ben@bendavidson.me>
2017-04-25 15:13:48 -0400

    Updated reference to FAAC parent theme

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'faac-incorporated-style';
+    $parent_style = 'faac-theme-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'faac-commercial-style',
